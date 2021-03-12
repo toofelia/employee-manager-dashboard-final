dc154439b00eb0ffc5e56108f47ea907000e928b
Ekaterina Mekshenkova <toofelia@gmail.com>
2021-03-11 22:46:09 -0700

    api update

--- client/js/dashboard/api/index.js
+++ client/js/dashboard/api/index.js
@@ -1,15 +1,15 @@
-// productionUrl = 'https://employee-manager-dashboard.herokuapp.com'
+// productionUrl = 'https://employee-manager-dashboard1.herokuapp.com/'
 // developmentUrl = 'http://localhost:5000/
 
 const api = {
-    getAllDepartments: 'http://localhost:5000/api/departments/',
+    getAllDepartments: 'https://employee-manager-dashboard1.herokuapp.com/api/departments/',
     // Snippet #5
-    updateEmployee: `http://localhost:5000/api/departments/employee/update`,
+    updateEmployee: `https://employee-manager-dashboard1.herokuapp.com/api/departments/employee/update`,
     getDepartment: (name)=>{
-        return `http://localhost:5000/api/departments/name/${name}`
+        return `https://employee-manager-dashboard1.herokuapp.com/api/departments/name/${name}`
     },
     deleteEmployee: (id)=>{
-        return `http://localhost:5000/api/departments/employee/id/${id}` 
+        return `https://employee-manager-dashboard1.herokuapp.com/api/departments/employee/id/${id}` 
     }
 }
 
